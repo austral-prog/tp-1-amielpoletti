0b4976349af16f068b1cf79eca5ca5e31b0f5de2
amielpoletti <162723122+amielpoletti@users.noreply.github.com>
2024-03-22 11:52:51 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,11 @@
-def math():
-    a = 57
-    b = 7
+def math ():
+	a = 57
+	b = 7
+	print( a + b )
+	print( a - b )
+	print( a * b )
+	print( (a + b)/2)
+	print( a // b ) # cociente entero
+	print( a % b ) # resto de una division entera
+	print( a / b ) # cociente real
+math()
